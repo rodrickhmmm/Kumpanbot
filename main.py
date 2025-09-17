298658ef1701b780085b020f5a949edaee2831b2
Rodrick__ <tom.rodrick100@gmail.com>
2025-09-17 18:03:17 +0200

    přidal jsem about me příkaz

--- main.py
+++ main.py
@@ -70,27 +70,16 @@ async def help_cmd(ctx: commands.Context):
     await ctx.reply(embed=embed)
 
 
-
-# Slash command version of help
-@bot.tree.command(name="prikazy", description="Ukáže přikázy které řinčák používá")
-async def help_slash(interaction: discord.Interaction):
-    embed = discord.Embed(title="Kumpánovské příkazy", color=discord.Color.purple())
+    
+@bot.tree.command(name="orincakovy", description="Řekne ti informace o botovy")
+async def about_slash(interaction: discord.Interaction):
+    embed = discord.Embed(title="O řinčákovy", color=discord.Color.purple())
     embed.set_thumbnail(url="https://images.uncyclomedia.co/necyklopedie/cs/thumb/f/f8/Frantisekreditel.jpg/250px-Frantisekreditel.jpg")
-    embed.add_field(name="můžeš buďto použít / a nebo k! k zadávání příkazů", value=" ", inline=False)
-    embed.add_field(name="/hraj <název|link>", value="Najde top 5 skladeb nebo přehraje hudbu z YouTube odkazu", inline=False)
-    embed.add_field(name="/preskocit", value="Přeskoč aktuální hudbu", inline=False)
-    embed.add_field(name="/prestat", value="Zastav aktuální hudbu a vymaže frontu", inline=False)
-    embed.add_field(name="/pauzni", value="Pozastavit nebo Pokračovat v hraní hudby", inline=False)
-    embed.add_field(name="/smycka", value="Zapne/vypne loopování hudby", inline=False)
-    embed.add_field(name="/fronta", value="Zobrazí frontu", inline=False)
-    embed.add_field(name="/nynihraje", value="Právě hraje", inline=False)
-    embed.add_field(name="/hlasitost <0-200>", value="Hlasitost", inline=False)
-    embed.add_field(name="/pripoj / /odpoj", value="Přivolej bota do voicu / Leavne voice", inline=False)
-    embed.add_field(name="/ping", value="Zkontroluj aktuální odezvu bota v milisekundách", inline=False)
-    embed.add_field(name="─────────────────────────────────────────────", value=" ", inline= False)
-    embed.add_field(name="Maty Mythic má oficiální zákaz používat tohoto bota", value=" ", inline=False)
-    embed.add_field(name="Platí také oficiální zákaz na mongolskej heavy metal, indickej phonk, čínskej rap a českej rap", value=" ", inline=False)
-    embed.add_field(name="Jestli si nemyslíš že Vráťa Hošek je nejlepší, tak toho bota rovnou smaž", value=" ", inline=False)
+    embed.add_field(name="Github link: https://github.com/rodrickhmmm/Kumpanbot", value=" ", inline=False)
+    embed.add_field(name="Jsem řinčák co je jen dostupný na Mým Kumpánům",value="",inline=False)
+    embed.add_field(name="Přehrávám hudbu a sloužím i na ostatní věci",value="",inline=False)
+    embed.add_field(name="Jsem forknutý z řinčáka Oguri, link: https://github.com/withoutminh/Oguri",value="",inline=False)
+    embed.add_field(name="Naprogramoval mě: původní kód - withoutminh, upravený a přidaný věci - Rodrick_ (rodrickhmmm) a Ocasník (xtomasnemec)")
     await interaction.response.send_message(embed=embed, ephemeral=True)
 
 
