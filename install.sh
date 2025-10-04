a2d63e924bbd2c04b6e0abccaf22123b3efc62f0
Tomáš Němec <40271551+xtomasnemec@users.noreply.github.com>
2025-10-04 21:08:02 +0200

    fix

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 
 sudo apt update
 sudo apt upgrade
-sudo apt install python3.11 ffmpeg nano -y
+sudo apt install python3.12 python3.12-venv ffmpeg nano -y
 
 python3.11 -m venv venv
 source venv/bin/activate
-python3.11 -m pip install --upgrade discord.py
-python3.11 -m pip install yt-dlp ffmpeg ffprobe PyNaCl
+python3.12 -m pip install --upgrade discord.py
+python3.12 -m pip install yt-dlp ffmpeg ffprobe PyNaCl
 echo "All required packages have been installed."
 clear
 read -s -p "Enter your token: " TOKEN
